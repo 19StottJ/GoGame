a8cfb4d0b7a12ff6b10ff5b9d2514941435615bb
Jack Stott <19StottJ@fulford.york.sch.uk>
2024-11-08 11:50:58 +0000

    Add project files.

---
+++ GoGame/Program.cs
@@ -0,0 +1,17 @@
+﻿using Raylib_cs;
+
+const int TILE_EMPTY = 0;
+const int PLAYER_SERVER = 1;
+
+const int WIDTH = 9;
+const int HEIGHT = 9;
+const int SCALE = 70;
+
+int[] tiles = new int[WIDTH * HEIGHT];
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    // draw tiles
+    Raylib.EndDrawing();
+}
